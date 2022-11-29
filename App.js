7b763293cab8178119801ef27c116e92d674d241
Dhruba Jyoti Talukdar <56720242+dhruba32@users.noreply.github.com>
2022-11-29 19:12:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState}  from './logo.svg';
+import './App.css';
+
+const App = () => {
+  //code here 
+  const [active,setActive]=useState(true);
+    return (
+      <div id="main">
+        
+       <p className={active?"redColor":"blueColor"}>Newton School</p>
+  
+        <button id='button' onClick={()=>setActive(!active)}>Change Style</button>
+      </div>
+    )
+  }
+
+export default App;
